296e66a11a1d715e7314cc2df463c1e6d17cbcf8
nholmesking <124222921+nholmesking@users.noreply.github.com>
2023-11-01 16:10:01 -0700

    More chindex

--- natus2json.py
+++ natus2json.py
@@ -344,7 +344,26 @@ chindex = {1: ['C3', 'C4', 'CZ', 'F3', 'F4', 'F7', 'F8', 'FZ', 'FP1', 'FP2',
            9: ['Ref', 'Fp1', 'F7', 'T3', 'A1', 'T5', 'O1', 'F3', 'C3', 'P3',
                'Fpz', 'Fz', 'Cz', 'Pz', 'Fp2', 'F8', 'T4', 'A2', 'T6', 'O2',
                'F4', 'C4', 'P4', 'X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7',
-               'X8', 'X9', 'X10']}
+               'X8', 'X9', 'X10'],
+           14: ['C3', 'C4', 'O1', 'O2', 'A1', 'A2', 'Cz', 'F3', 'F4', 'F7',
+                'F8', 'Fz', 'Fp1', 'Fp2', 'Fpz', 'P3', 'P4', 'Pz', 'T3', 'T4',
+                'T5', 'T6', 'LOC', 'ROC', 'CHIN1', 'CHIN2', 'ECGL', 'ECGR',
+                'LAT1', 'LAT2', 'RAT1', 'RAT2', 'CHEST', 'ABD', 'FLOW',
+                'SNORE', 'DIF5', 'DIF6', 'POS', 'DC2', 'DC3', 'DC4', 'DC5',
+                'DC6', 'DC7', 'DC8', 'DC9', 'DC10', 'OSAT', 'PR'],
+           15: ['Fp1', 'F7', 'T3', 'A1', 'T5', 'O1', 'F3', 'C3', 'P3', 'Fpz',
+                'Fz', 'Cz', 'Pz', 'Fp2', 'F8', 'T4', 'A2', 'T6', 'O2', 'F4',
+                'C4', 'P4', 'X1', 'X2', 'DIF1', 'DIF2', 'DIF3', 'DIF4', 'DC1',
+                'DC2', 'DC3', 'DC4', 'OSAT', 'PR'],
+           17: ['Fp1', 'F7', 'T3', 'T5', 'O1', 'F3', 'C3', 'P3', 'A1', 'Fz',
+                'Cz', 'Fpz', 'Pz', 'X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7',
+                'X8', 'X9', 'Fp2', 'X10', 'F8', 'X11', 'T4', 'X12', 'T6',
+                'X13', 'O2', 'X14', 'F4', 'X15', 'C4', 'X16', 'P4', 'X17',
+                'A2', 'X18', 'DC1', 'DC2', 'DC3', 'DC4', 'OSAT', 'PR'],
+           19: ['C3', 'C4', 'CZ', 'F3', 'F4', 'F7', 'F8', 'FZ', 'FP1', 'FP2',
+                'FPZ', 'O1', 'O2', 'P3', 'P4', 'PZ', 'T3', 'T4', 'T5', 'T6',
+                'AUX1', 'AUX2', 'AUX3', 'AUX4', 'AUX5', 'AUX6', 'AUX7', 'AUX8',
+                'PG1', 'PG2', 'A1', 'A2']}
 
 
 def natus2json(filename, jsonname):
