3ca293b64c8f443ce1b41b65bdb7a73b4c508232
nholmesking <124222921+nholmesking@users.noreply.github.com>
2023-11-06 11:59:05 -0800

    rightpad(), more placeholders

--- json2edf.py
+++ json2edf.py
@@ -15,6 +15,13 @@ PEP-8 compliant.
 """
 
 
+def rightpad(ins, n):
+    out = ins
+    while len(out) < n:
+        out += ' '
+    return out
+
+
 def json2edf(jsonname, edfname):
     infile = open(jsonname, 'r')
     jinp = json.loads(infile.read())
@@ -31,10 +38,16 @@ def json2edf(jsonname, edfname):
         s += 'X'
     s += ' '
     # MORE CODE HERE
-    while len(s) < 80:
-        s += ' '
+    s = rightpad(s, 80)
     edffile.write(s[:80])
     edffile.write(' ' * 80)  # PLACEHOLDER, local recording identification
+    edffile.write(' ' * 8)  # PLACEHOLDER, startdate
+    edffile.write(' ' * 8)  # PLACEHOLDER, starttime
+    edffile.write(rightpad(str(256 + jinp['m_num_channels'] * 256), 8))
+    edffile.write(' ' * 44)  # PLACEHOLDER, reserved
+    edffile.write(' ' * 8)  # PLACEHOLDER, number of data records
+    edffile.write(' ' * 8)  # PLACEHOLDER, duration of a data record
+    edffile.write(rightpad(str(jinp['m_num_channels']), 8))
     # MORE CODE HERE
     edffile.close()
 
