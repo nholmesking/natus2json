446d312646eef628928e29ef54060618b94f023f
nholmesking <124222921+nholmesking@users.noreply.github.com>
2023-11-13 13:19:27 -0800

    Create json2csv.py

---
+++ json2csv.py
@@ -0,0 +1,40 @@
+# Nathan Holmes-King
+# 2023-11-13
+
+import sys
+import json
+import csv
+
+"""
+WORK IN PROGRESS.
+
+Command-line arguments:
+1. JSON file name
+2. CSV file name
+
+PEP-8 compliant.
+"""
+
+
+def json2csv(jsonname, csvname):
+    infile = open(jsonname, 'r')
+    jinp = json.loads(infile.read())
+    infile.close()
+    csvfile = open(csvname, 'w')
+    csvwriter = csv.writer(csvfile)
+    firstrow = ['index', 'event_byte']
+    for b in jinp['data'][0]['delta_information']:
+        firstrow.append(b)
+    csvwriter.writerow(firstrow)
+    k = 0
+    while k < len(jinp['data']):
+        row = [k, jinp['data'][k]['event_byte']]
+        for a in jinp['data'][k]['delta_information']:
+            row.append(jinp['data'][k]['delta_information'][a])
+        csvwriter.writerow(row)
+        k += 1
+    csvfile.close()
+
+
+if __name__ == '__main__':
+    json2csv(sys.argv[1], sys.argv[2])
