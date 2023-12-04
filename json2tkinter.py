13cbbceddc1ecc98d1d062e5802766e514c207b3
nholmesking <124222921+nholmesking@users.noreply.github.com>
2023-12-04 15:58:05 -0800

    Support for Tkinter scrolling

--- json2tkinter.py
+++ json2tkinter.py
@@ -11,26 +11,55 @@ WORK IN PROGRESS.
 Command-line arguments:
 1. JSON file name.
 
+How to use:
+- Left & right arrow keys to scroll time range.
+
 PEP-8 compliant.
 """
 
 
-def json2tkinter(jsonname):
-    infile = open(jsonname, 'r')
-    jinp = json.loads(infile.read())
-    infile.close()
-    tk = Tk()
-    tk.title('Natus visualization')
-    c = Canvas(height=900, width=1500)
-    c.pack()
+jinp = {}
+tk = 0
+c = 0
+xoff = 0
+yoff = 0
+
+
+def scrollRight(event):
+    global jinp
+    global tk
+    global c
+    global xoff
+    global yoff
+    xoff += 10
+    c.delete('all')
+    draw(tk, c, jinp['data'], xoff, yoff)
+
+
+def scrollLeft(event):
+    global jinp
+    global tk
+    global c
+    global xoff
+    global yoff
+    if xoff >= 10:
+        xoff -= 10
+    c.delete('all')
+    draw(tk, c, jinp['data'], xoff, yoff)
+
+
+def draw(tk, c, data, xoff, yoff):
+    """
+    Draws everything on canvas.
+    """
     c.create_line(100, 100, 1500, 100)  # Top line
     c.create_line(100, 100, 100, 900)  # Left line
     for i in range(0, 15):
         c.create_line(100*(i+1), 80, 100*(i+1), 100)
-        c.create_text(100*(i+1), 75, text=str(10*i), font=('Helvetica', 32),
-                      anchor='s')
+        c.create_text(100*(i+1), 75, text=str(10*(i+int(xoff/10))),
+                      font=('Helvetica', 32), anchor='s')
     sensors = []
-    for a in jinp['data'][0]['delta_information']:
+    for a in data[0]['delta_information']:
         sensors.append(a)
     for i in range(len(sensors)):
         c.create_line(80, 200*(i+1), 100, 200*(i+1))
@@ -39,12 +68,33 @@ def json2tkinter(jsonname):
         for j in range(0, 140):
             c.create_line(j*10+100,
                           (200*(i+1) -
-                           jinp['data'][j]['delta_information'][sensors[i]] /
+                           data[xoff+j]['delta_information'][sensors[i]] /
                            50), j*10+110,
                           (200*(i+1) -
-                           jinp['data'][j+1]['delta_information'][sensors[i]] /
+                           data[xoff+j+1]['delta_information'][sensors[i]] /
                            50), width=2)
     tk.update()
+
+
+def json2tkinter(jsonname):
+    """
+    Main function. Reads data, sets up Tk, and calls helpers.
+    """
+    global jinp
+    global tk
+    global c
+    global xoff
+    global yoff
+    infile = open(jsonname, 'r')
+    jinp = json.loads(infile.read())
+    infile.close()
+    tk = Tk()
+    tk.title('Natus visualization')
+    c = Canvas(height=900, width=1500)
+    c.pack()
+    c.bind_all('<KeyPress-Right>', scrollRight)
+    c.bind_all('<KeyPress-Left>', scrollLeft)
+    draw(tk, c, jinp['data'], 0, 0)
     inp = ''
     while inp != 'y':
         inp = input('Exit? (y/[n])')
