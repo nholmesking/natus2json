6b048f9bddb540c5f8638528cc9d72e51aa9e216
nholmesking <124222921+nholmesking@users.noreply.github.com>
2023-12-11 09:51:42 -0800

    Update json2dicom.py

--- json2dicom.py
+++ json2dicom.py
@@ -19,6 +19,9 @@ PEP-8 compliant.
 
 
 def json2dicom(jsonname, dicomname):
+    fex = jsonname[len(jsonname)-8:len(jsonname)-5]
+    if fex == 'old':
+        fex = jsonname[len(jsonname)-12:len(jsonname)-9]
     infile = open(jsonname, 'r')
     jinp = json.loads(infile.read())
     infile.close()
@@ -28,11 +31,26 @@ def json2dicom(jsonname, dicomname):
     ds.PatientName = (jinp['m_pat_last_name'] + '^' +
                       jinp['m_pat_first_name'] + '^' +
                       jinp['m_pat_middle_name'])
-    ds.PatientID = jinp['m_pat_id']  # VERIFY
-    ds.CreationDate = valuerep.DA.fromisoformat(jinp['m_creation_time'][:10])
-    ds.CreationTime = (valuerep.TM.
-                       fromisoformat(jinp['m_creation_time'][11:19] +
-                                     '+00:00'))
+    ds.PatientID = jinp['m_pat_id']
+    ds.InstanceCreationDate = (valuerep.DA.
+                               fromisoformat(jinp['m_creation_time'][:10]))
+    ds.InstanceCreationTime = (valuerep.TM.
+                               fromisoformat(jinp['m_creation_time'][11:19] +
+                                             '+00:00'))
+    ins = ''
+    i = 0
+    while i < len(jinp['m_file_guid']):
+        if jinp['m_file_guid'][i] == '-':
+            i += 1
+            continue
+        if len(ins) > 0:
+            ins += '.'
+        ins += str(int(jinp['m_file_guid'][i:i+2], 16))
+        i += 2
+    ds.SOPInstanceUID = ins  # VERIFY
+    ds.Modality = 'EEG'
+    if fex == 'erd':
+        ds.SamplingFrequency = jinp['m_sample_freq']
     # EXPAND
     filewriter.dcmwrite(dicomname, ds)
 
