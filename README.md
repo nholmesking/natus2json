f4b5e0dc31b6a82493643cc40b9629aafb3a6b35
nholmesking <124222921+nholmesking@users.noreply.github.com>
2023-12-16 12:48:17 -0800

    Create dicom2edf.py

--- README.md
+++ README.md
@@ -10,9 +10,10 @@ Files:
 - json2csv.py: converts JSON (from Natus ERD files) to CSV.
 - edf_range.py: inputs an EDF and outputs a selected time range from the EDF.
 - json2tkinter.py: inputs a JSON (from Natus ERD files) and generates an EEG visualization using Tkinter.
-- json2dicom.py: inputs a JSON (from natus2json.py) and generates a DICOM file.
+- json2dicom.py: inputs a JSON (from natus2json.py) and generates a DICOM file. Currently non-functional.
 - json2dicom_altered.py: inputs a JSON (from Natus ERD files) and generates a nonstandard version of DICOM that uses less than half the amount of memory as the standard version.
 - edf2dicom.py: inputs an EDF and converts it to standard DICOM. Assumes EDF+ from Natus ERD.
+- dicom2edf.py: reverses edf2dicom.py. Currently does nothing useful.
 
 See also:
 - Document outlining results: https://docs.google.com/document/d/1vsgSZQMatsJYphpQt628trsgjX6-iKG2NcAN6Uziidk/edit?usp=sharing
---
+++ dicom2edf.py
@@ -0,0 +1,70 @@
+# Nathan Holmes-King
+# 2023-12-16
+
+import sys
+from pydicom import dcmread
+import time
+from json2edf import rightpad
+
+"""
+WORK IN PROGRESS.
+
+Command-line arguments:
+1. Input file name.
+2. Output file name.
+
+PEP-8 compliant.
+"""
+
+
+months = {1: 'JAN',
+          2: 'FEB',
+          3: 'MAR',
+          4: 'APR',
+          5: 'MAY',
+          6: 'JUN',
+          7: 'JUL',
+          8: 'AUG',
+          9: 'SEP',
+          10: 'OCT',
+          11: 'NOV',
+          12: 'DEC'}
+
+
+def dicom2edf(dicomname, edfname):
+    ds = dcmread(dicomname, force=True)
+    outfile = open(edfname, 'wb')
+    outfile.write(bytes(' ' * 8, 'ascii'))  # PLACEHOLDER
+    lpid = (ds.PatientID + ' ' + ds.PatientSex + ' ' +
+            ds.PatientBirthDate[6:8] + '-' +
+            months[int(ds.PatientBirthDate[4:6])] + '-' +
+            ds.PatientBirthDate[:4] + ' ')
+    try:
+        for a in ds.PatientName:
+            if a == '^':
+                lpid += '_'
+            else:
+                lpid += a
+    except AttributeError:  # Patient name not listed
+        pass
+    outfile.write(rightpad(lpid, 80))
+    outfile.write(bytes(' ' * 80, 'ascii'))  # PLACEHOLDER
+    adt = ds.AcquisitionDateTime
+    outfile.write(bytes(adt[6:8] + '.' + adt[4:6] + '.' + adt[2:4] +
+                        adt[8:10] + '.' + adt[10:12] + '.' +
+                        adt[12:14], 'ascii'))
+    outfile.write(rightpad(str(256 * ds.WaveformSequence[0].
+                               NumberOfWaveformChannels + 256), 8))
+    outfile.write(bytes(' ' * 44, 'ascii'))
+    outfile.write(rightpad(str(len(ds.WaveformSequence)), 8))
+    outfile.write(rightpad('1', 8))  # VERIFY
+    outfile.write(rightpad(str(ds.WaveformSequence[0].
+                               NumberOfWaveformChannels), 4))
+    # EXPAND
+    outfile.close()
+
+
+if __name__ == '__main__':
+    t = time.time()
+    dicom2edf(sys.argv[1], sys.argv[2])
+    print('DONE', round(time.time() - t, 2), 's')
