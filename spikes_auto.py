b793bdc70bbe0a75ea5e655a5eca114bbad7690f
nholmesking <124222921+nholmesking@users.noreply.github.com>
2024-03-16 17:02:49 -0700

    Update spikes_auto.py

--- spikes_auto.py
+++ spikes_auto.py
@@ -7,6 +7,7 @@ import sys
 import os
 import csv
 import scipy
+import numpy as np
 
 """
 WORK IN PROGRESS.
@@ -16,6 +17,9 @@ Command-line arguments:
 
 PEP-8 compliant.
 """
+
+
+bad = {'IED-41.EDF': ['EEG C3-M2']}
 
 
 def findSpikesQS(raw, f, wrt):
@@ -30,32 +34,78 @@ def findSpikesQS(raw, f, wrt):
             wrt.writerow([f, raw.ch_names[i], len(tm)])
 
 
-def findSpikesSciPy(raw, f, wrt):
+def findSpikesSciPy(raw, f, wrt, height=None, thresh=None, prom=None):
     nch = len(raw.ch_names)
     srate = int(raw.info['sfreq'])
     data, times = raw.get_data(return_times=True)
+    rl = []
     for i in range(nch):
         if len(raw.ch_names[i]) > 2 and raw.ch_names[i][:3] == 'EEG':
-            peaks, _ = scipy.signal.find_peaks(data[i], height=1e-3)
+            peaks, _ = scipy.signal.find_peaks(data[i], height=height,
+                                               threshold=thresh,
+                                               prominence=prom)
             wrt.writerow([f, raw.ch_names[i], len(peaks)])
+            if (not f in bad) or (not raw.ch_names[i] in bad[f]):
+                rl.append(len(peaks))
+            """
+            outfile = open('spikes_' + f + '_' + raw.ch_names[i] + '.svg', 'w')
+            outfile.write('<!DOCTYPE svg>\n<svg width="100000" height="2000" '
+                          'xmlns="http://www.w3.org/2000/svg">')
+            for j in range(len(data[i])-1):
+                if j > 1e5:
+                    break
+                outfile.write('\n\t<line x1="' + str(j) + '" x2="' + str(j+1) +
+                              '" y1="' + str(1e3 - data[i][j]*5e5) + '" y2="' +
+                              str(1e3 - data[i][j+1]*5e5) +
+                              '" stroke="#000"/>')
+            for j in range(len(peaks)):
+                if peaks[j] > 1e5:
+                    break
+                outfile.write('\n\t<circle cx="' + str(peaks[j]) + '" cy="' +
+                              str(1e3 - data[i][peaks[j]]*5e5) +
+                              '" r="5" fill="#f00" stroke="#000"/>')
+            outfile.write('\n</svg>')
+            outfile.close()
+            """
+    return rl
 
 
 def main(indir, outf):
-    outp = open(outf, 'w')
-    wrt = csv.writer(outp)
-    wrt.writerow(['File', 'Channel', 'Number of spikes'])
-    for f in os.listdir(indir):
-        try:
-            raw = mne.io.read_raw_edf(indir + '/' + f, preload=True,
-                                      verbose='ERROR')
-        except TypeError:
-            continue
-        except NotImplementedError:
-            continue
-        except ValueError:
-            continue
-        findSpikesSciPy(raw, f, wrt)
-    outp.close()
+    tout = open(outf, 'w')
+    rit = csv.writer(tout)
+    rit.writerow(['Height', 'Threshold', 'Prominence', 'Score'])
+    for h in [0, 1e-4, 2e-4, 5e-4, 1e-3]:
+        for t in [0, 1e-4, 2e-4, 5e-4, 1e-3]:
+            for p in [0, 1e-4, 2e-4, 5e-4, 1e-3]:
+                dic = {True: [], False: []}
+                outp = open('spikes_' + str(h) + '_' + str(t) + '_' + str(p) +
+                            '.csv', 'w')
+                wrt = csv.writer(outp)
+                wrt.writerow(['Height', 'Channel', 'Number of spikes'])
+                for f in os.listdir(indir):
+                    try:
+                        raw = mne.io.read_raw_edf(indir + '/' + f,
+                                                  preload=True,
+                                                  verbose='ERROR')
+                    except TypeError:
+                        continue
+                    except NotImplementedError:
+                        continue
+                    except ValueError:
+                        continue
+                    rl = findSpikesSciPy(raw, f, wrt, height=h, thresh=t,
+                                         prom=p)
+                    print('DONE', h, t, p, f)
+                    if int(f.split('.')[0].split('-')[1]) < 40:
+                        for a in rl:
+                            dic[True].append(a)
+                    else:
+                        for a in rl:
+                            dic[False].append(a)
+                outp.close()
+                rit.writerow([h, t, p, np.log(np.mean(dic[True]) /
+                                              np.mean(dic[False]))])
+    tout.close()
 
 
 if __name__ == '__main__':
