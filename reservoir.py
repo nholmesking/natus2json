ada19272bc9f8abc9593270287a70f30d92a51ca
nholmesking <124222921+nholmesking@users.noreply.github.com>
2024-05-01 16:43:34 -0700

    Update reservoir.py

--- reservoir.py
+++ reservoir.py
@@ -24,6 +24,7 @@ std_ch = ['EEG F3-M2', 'EEG F4-M1', 'EEG T3-M2', 'EEG T4-M1']
 def main(indir):
     X = []
     y = []
+    max_len = 0
     for f in os.listdir(indir):
         # Read data
         try:
@@ -51,15 +52,30 @@ def main(indir):
         X.append(np.array(Xtemp))
         # Spikes or not?
         if int(f.split('.')[0][4:]) > 40:
-            y.append(np.array([False]))
+            y.append(np.array([0]))
         else:
-            y.append(np.array([True]))
+            y.append(np.array([1]))
+        # Update max_len
+        if raw.n_times > max_len:
+            max_len = raw.n_times
+    # Final pre-processing
+    for i in range(len(X)):
+        if X[i].shape[1] < max_len:
+            X[i] = np.append(X[i], np.zeros((X[i].shape[0],
+                                             max_len-X[i].shape[1])), axis=1)
+    newX = np.empty(shape=(len(X),), dtype=object)
+    newY = np.empty(shape=(len(y),), dtype=object)
+    for i in range(len(X)):
+        newX[i] = X[i]
+        newY[i] = y[i]
+    X = newX
+    y = newY
+    # Train model
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
     clf = ESNClassifier()
     clf.fit(X=X_train, y=y_train)
     y_pred_classes = clf.predict(X=X_test)
-    y_pred_proba = clf.predict_proba(X=X_test)
-    print(y_pred_classes.shape)  # TEMP
+    print('test', y_test, y_pred_classes == y_test)
 
 
 if __name__ == '__main__':
